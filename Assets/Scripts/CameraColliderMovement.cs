247b31ed87073c0f087cd23029ab0b78d9cf1924
Derek <DGL170030@utdallas.edu>
2021-11-09 22:53:12 -0600

    Map and Camera

---
+++ Assets/Scripts/CameraColliderMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraColliderMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController _fleshPrison;
+    [SerializeField] Collider _mapCollider;
+
+    public float _speed;
+    public float _verticalSpeedWeight;
+
+    private bool _isInMap;
+    private Vector3 origin = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distFromZero = Vector3.Distance(origin, transform.position);
+        if (transform.position.y <= 0 && distFromZero <= 28) 
+        {
+            _mapCollider.enabled = true;
+            _isInMap = true;
+        }
+        else
+        {
+            _mapCollider.enabled = false;
+            _isInMap = false;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+        if (Input.GetButton("Jump"))
+        {
+            move.y = 1 * _verticalSpeedWeight;
+        }        
+        if (Input.GetKey(KeyCode.LeftControl) && _isInMap == false)
+        {
+            if(transform.position.y - 1 > 0 || distFromZero >= 28)
+            {
+                move.y = -1 * _verticalSpeedWeight;
+            }
+        }
+        _fleshPrison.Move(move * _speed * Time.deltaTime);//movement apply
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivityX = 100f;
+    public float mouseSensitivityY = 100f;
+    [SerializeField] Transform _brainCage;
+
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //if (_level1Controll._isInPauseMenu != true)
+        //{
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            _brainCage.Rotate(Vector3.up * mouseX);
+            Cursor.visible = false;
+        //}
+        //else
+        //{
+            //Cursor.lockState = CursorLockMode.None;
+            //Cursor.visible = true;
+        //}
+    }
+}
