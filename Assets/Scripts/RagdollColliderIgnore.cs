e97d9df1b511ccce8b577b8388716c468fbbf572
Derek <DGL170030@utdallas.edu>
2021-11-29 15:49:36 -0600

    Fixed collision between multiple ragdolls, fixed body weight

---
+++ Assets/Scripts/RagdollColliderIgnore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollColliderIgnore : MonoBehaviour
+{
+    [SerializeField] bool _ignoreGrandparent = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(transform.parent != null)
+        {
+            Debug.Log("Part " + transform.name + " parent: " + transform.parent.name);
+            if(transform.parent.GetComponent<Collider>() != null)
+            {
+                Physics.IgnoreCollision(transform.parent.GetComponent<Collider>(), GetComponent<Collider>(), true);
+            }
+        }   
+        if(transform.childCount > 0)
+        {
+            Debug.Log("Part " + transform.name + " child count: " + transform.childCount);
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                if(transform.GetChild(i).GetComponent<Collider>() != null)
+                {
+                    Physics.IgnoreCollision(transform.GetChild(i).GetComponent<Collider>(), GetComponent<Collider>(), true);
+                }
+            }
+        }
+
+        if(_ignoreGrandparent == true)
+        {
+            Physics.IgnoreCollision(transform.parent.parent.GetComponent<Collider>(), GetComponent<Collider>(), true);
+        }
+    }
+}
