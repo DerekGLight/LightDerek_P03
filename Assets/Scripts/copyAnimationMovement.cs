1503547e3acf5ca7c454a01e6284e1e69e66f021
Derek <DGL170030@utdallas.edu>
2021-11-30 18:34:19 -0600

    Added walking animation, fixed feet colliders

---
+++ Assets/Scripts/copyAnimationMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyAnimationMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _targetJoint;
+    [SerializeField] private bool _ClampBackwardsMovement;
+
+    ConfigurableJoint _thisJoint;
+    Quaternion targetInitialRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _thisJoint = GetComponent<ConfigurableJoint>();
+        targetInitialRotation = _targetJoint.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _thisJoint.targetRotation = copyRotation();
+    }
+
+    private Quaternion copyRotation()
+    {
+        return Quaternion.Inverse(_targetJoint.localRotation) * targetInitialRotation;
+    }
+}
